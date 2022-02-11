033835b2c8d94d2114e81a75d7506c3ed6688738
Pete <pete81833@gmail.com>
2022-02-11 10:17:48 +0800

    第一次建立檔案

---
+++ MyDestiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDestiny
+//
+//  Created by 胡丕 on 2022/2/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
