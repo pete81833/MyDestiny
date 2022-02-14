1a8cb0c3446cd4ba64a60fc63cc6526b0eb0c6ce
Pete <pete81833@gmail.com>
2022-02-14 15:40:10 +0800

    loginButton 設定完成

--- MyDestiny/LoginViewController.swift
+++ MyDestiny/LoginViewController.swift
@@ -7,17 +7,20 @@
 
 import UIKit
 import MHLoadingButton
+import FirebaseAuth
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var loginBtn: LoadingButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginBtn.cornerRadius = 20
+        self.loginBtn.indicator = BallPulseIndicator(color: .black)
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func login(_ sender: LoadingButton) {
-        
         if sender.isLoading {
             sender.hideLoader()
         } else {
@@ -27,5 +30,13 @@ class LoginViewController: UIViewController {
         
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
